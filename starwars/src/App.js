8493adacc1f4f61a0d62f210cf5cf75a60b904f0
Tharshan Ketheeswaran <tketheeswaran09@gmail.com>
2020-06-29 22:05:30 +0100

    Donesprintt

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import Character from "./components/Character";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [state, setState] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://swapi.py4e.com/api/people/").then((response) => {
+      setState(response.data.results);
+      console.log(response.data.results);
+    });
+  }, []);
+  const mapData = state.map((item) => (
+    <Character key={item.name} characterData={item} />
+  ))
+
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -11,7 +25,8 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">About Star Wars Characters </h1>
+      {mapData}
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,27 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const DivBackground = styled.div`
+  border: 4px solid grey;
+  margin: 20px 300px;
+  padding: 5px;
+  background-color: white;
+`;
+
+const NameBackground = styled.h2`
+  color: #2b3464;
+`;
+
+const Character = (props) => {
+    return (
+        <DivBackground>
+            <NameBackground>{props.characterData.name}</NameBackground>
+            <p> height: {Math.round(props.characterData.height / 30.5)} ft</p>
+            <p> weight: {Math.round(props.characterData.mass * 2.2)} lbs</p>
+            <p> eye color: {props.characterData.eye_color}</p>
+        </DivBackground>
+    );
+};
+
+export default Character;
